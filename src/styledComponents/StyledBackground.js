e1d2d0de6323859077631170b1a1326dffcf7a84
Simon Godefroy <godefroysimon@laposte.net>
2019-07-21 13:51:08 +0200

    no background repeat

--- src/styledComponents/StyledBackground.js
+++ src/styledComponents/StyledBackground.js
@@ -11,33 +11,34 @@ import style from './style';
 
 const StyledBackground = styled(StyledSection)`
   padding: 0;
+  background-image: url(${mobileSBackground});
+  height: 427px;
   @media (min-width: ${style.mediaSize.mobileS}) {
-    background-image: url(${mobileSBackground});
-    height: 427px;
-  }
-  @media (min-width: ${style.mediaSize.mobileM}) {
     background-image: url(${mobileMBackground});
     height: 500px;
   }
-  @media (min-width: ${style.mediaSize.mobileL}) {
+  @media (min-width: ${style.mediaSize.mobileM}) {
     background-image: url(${mobileLBackground});
     height: 567px;
   }
-  @media (min-width: ${style.mediaSize.tablet}) {
+  @media (min-width: ${style.mediaSize.mobileL}) {
     background-image: url(${tabletBackground});
     height: 576px;
   }
-  @media (min-width: ${style.mediaSize.laptop}) {
+  @media (min-width: ${style.mediaSize.tablet}) {
     background-image: url(${laptopBackground});
     height: 768px;
   }
-  @media (min-width: ${style.mediaSize.laptopL}) {
+  @media (min-width: ${style.mediaSize.laptop}) {
     background-image: url(${laptopLBackground});
     height: 1080px;
   }
-  @media (min-width: ${style.mediaSize.desktop}) {
+  @media (min-width: ${style.mediaSize.laptopL}) {
     background-image: url(${desktopBackground});
     height: 1920px;
+  }
+  @media (min-width: ${style.mediaSize.desktop}) {
+    background-repeat: round;
   }
 `
 
