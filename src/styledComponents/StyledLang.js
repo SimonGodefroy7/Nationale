270f8859a6278780a74749deb47a9c1e23765005
Simon Godefroy <godefroysimon@laposte.net>
2019-07-22 15:15:38 +0200

    Change translation and add space between the two flags

--- src/styledComponents/StyledLang.js
+++ src/styledComponents/StyledLang.js
@@ -4,6 +4,9 @@ import StyledImg from './StyledImg';
 
 const StyledTabletBox = styled(StyledFlexBox)`
   flex-direction: row;
+  ${StyledFlexBox} {
+    margin: 0.2rem;
+  }
   ${StyledImg} {
     width: 2rem;
     height: 1rem;
