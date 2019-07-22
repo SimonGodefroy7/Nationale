aaf4a4693d3789bfde718e2f685178bc2f38b3c4
Simon Godefroy <godefroysimon@laposte.net>
2019-07-22 15:16:01 +0200

    Updates

--- precache-manifest.5c67fd8d9addfdab9c907c738fae87f0.js
+++ precache-manifest.5c67fd8d9addfdab9c907c738fae87f0.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3cbac36e3d61b74adf3124dc12b5e9f7",
+    "revision": "14f455bc927cab8f3cf0be2d463d595d",
     "url": "/Nationale/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Nationale/static/js/2.bc3fe03c.chunk.js"
   },
   {
-    "revision": "8d7de7bb45de328b2a08",
-    "url": "/Nationale/static/js/main.d567f023.chunk.js"
+    "revision": "5789fe99caf21c859cc9",
+    "url": "/Nationale/static/js/main.3e45a242.chunk.js"
   },
   {
     "revision": "7665ca69f4be525e3cfd",
