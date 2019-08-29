19f0ac82848d1ebc4cac16c94c59a868c4b425b8
Simon Godefroy <godefroysimon@laposte.net>
2019-08-29 16:53:31 +0200

    make deploy work

--- src/styledComponents/StyledContactBox.js
+++ src/styledComponents/StyledContactBox.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import StyledBorderBox from './StyledBorderBox';
 //import style from './style';
-
+//
 const StyledContactBox = styled(StyledBorderBox)`
 
 `
