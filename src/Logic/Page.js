43c617c955c157dfcb92bf6b303c358769740d1e
Simon Godefroy <godefroysimon@laposte.net>
2019-10-28 22:05:58 +0100

    comment footer import

--- src/Logic/Page.js
+++ src/Logic/Page.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Header from './Header'
-import Footer from './Footer'
+//import Footer from './Footer'
 import StyledFlexBox from '../styledComponents/StyledFlexBox';
 
 class Page extends Component {
