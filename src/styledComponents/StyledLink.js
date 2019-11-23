af0df51ac241cabca8893fe784f753ccb8656ad5
BrunoGodefroy <godefroy.bruno77@gmail.com>
2019-11-23 18:53:27 +0100

    Wrap the react router link to make sure no unexpected props can be passed to the DOM

--- src/styledComponents/StyledLink.js
+++ src/styledComponents/StyledLink.js
@@ -1,7 +1,10 @@
+import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-const StyledLink = styled(Link)`
+const SafellyWrappedLink = ({ hoverless, paddingless, ...rest }) => <Link {...rest} />;
+
+const StyledLink = styled(SafellyWrappedLink)`
   color: ${props => props.hoverless ? '' : 'green'};
   text-decoration: none;
   padding: ${props => props.paddingless ? '0' : '0.5rem'};
@@ -11,6 +14,6 @@ const StyledLink = styled(Link)`
     background-color: ${props => props.hoverless ? '' : 'green'};
     border-radius: ${props => props.hoverless ? '' : '0.5rem'};
   }
-`
+`;
 
 export default StyledLink;
