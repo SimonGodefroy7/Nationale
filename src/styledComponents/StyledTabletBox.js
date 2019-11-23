5fcf2580b4457194a340db1640817600fe29d042
BrunoGodefroy <godefroy.bruno77@gmail.com>
2019-11-23 19:08:50 +0100

    Add padding and border radius to the main homepage headline

--- src/styledComponents/StyledTabletBox.js
+++ src/styledComponents/StyledTabletBox.js
@@ -5,18 +5,17 @@ import style from './style';
 
 const StyledTabletBox = styled(StyledFlexBox)`
   background-color: #ef7d28;
-  width: 8rem;
+  width: 10rem;
   margin-top: 1rem;
   margin-bottom: 1rem;
+  padding: 1rem;
+  border-radius: 1rem;
+
   ${StyledH1} {
     text-align: center;
+    font-size: ${style.fontSize.small};
   }
-  @media (min-width: ${style.mediaSize.mobileS}) {
-    ${StyledH1} {
-      font-size: ${style.fontSize.small};
-    }
-    width: 10rem;
-  }
+
   @media (min-width: ${style.mediaSize.mobileM}) {
     ${StyledH1} {
       font-size: ${style.fontSize.small};
