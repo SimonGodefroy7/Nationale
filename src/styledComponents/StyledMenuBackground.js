910b7bac260a88f8cedac59a734aefbe5796ab0e
BrunoGodefroy <godefroy.bruno77@gmail.com>
2019-11-23 18:59:11 +0100

    Fix design of the main header to make sure it stays always centered

--- src/styledComponents/StyledMenuBackground.js
+++ src/styledComponents/StyledMenuBackground.js
@@ -2,24 +2,18 @@ import styled from 'styled-components';
 import StyledFlexBox from './StyledFlexBox';
 import StyledH1 from './StyledH1';
 import StyledH1Short from './StyledH1Short';
-import style from './style';
 
 const StyledMenuBackground = styled(StyledFlexBox)`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
+
   ${StyledH1} {
     color: #ef7d28;
   }
   ${StyledH1Short} {
     color: #ef7d28;
   }
-  @media (min-width: ${style.mediaSize.tablet}) {
-    align-items: unset;
-  }
-  @media (min-width: ${style.mediaSize.laptop}) {
-    align-items: center;
-  }
-`
+`;
 
 export default StyledMenuBackground;
