0dd886d081757c6e4a39f881538582eeb065ab76
Simon Godefroy <godefroysimon@laposte.net>
2020-07-30 17:53:03 +0200

    update lastupdate date

--- src/functions/lastUpdate.js
+++ src/functions/lastUpdate.js
@@ -2,7 +2,7 @@ const lastUpdate = {
   PARTNERS: "19/07/2020",
   RESULTS: "01/07/2020",
   STARTLIST: "01/07/2020",
-  TRAINING: "19/07/2020",
+  TRAINING: "30/07/2020",
   SPRINTEVENT: "25/07/2020",
   MDEVENT: "19/07/2020",
   LDEVENT: "19/07/2020",
@@ -10,7 +10,7 @@ const lastUpdate = {
   GALLERY: "19/07/2020",
   CONTACT: "19/07/2020",
   LEGO: "25/07/2020",
-  HOME: "19/07/2020"
+  HOME: "30/07/2020",
 };
 
 export default lastUpdate;
